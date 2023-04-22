5e8219ae325d20076728dec58b0548a71cb153bf
Wes Braga <wesbragagt@gmail.com>
2023-04-21 21:04:19 -0500

    feat(1.install-vite-react-plugin): part 1

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import {defineConfig} from 'vite';
+import reactPlugin from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [
+    reactPlugin()
+  ],
+  publicDir: './public'
+})
