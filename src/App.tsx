7d232360f43a31fca4296af7709ff68e4e601368
Wes Braga <wesbragagt@gmail.com>
2023-04-21 20:17:41 -0500

    feat(1.install-vite-react-plugin): part 1

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+<<<<<<< HEAD
           Edit this with Vite and experience speed
         </p>
         <p>
